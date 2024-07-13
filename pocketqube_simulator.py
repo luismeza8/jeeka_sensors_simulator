f7f8ff9d20bfe0cfcd66529ef5918fb7dc2bc905
luismeza8 <angel841bv@gmail.com>
2024-07-12 21:59:29 -0700

    requests to endpoint to save meditions

--- pocketqube_simulator.py
+++ pocketqube_simulator.py
@@ -2,94 +2,108 @@ import serial
 import random
 import pandas as pd
 
-serial_port_1 = serial.Serial('/dev/pts/6')
-csv_data = pd.read_csv('datos_corregidos.csv')
-iteration = 0
+class PocketQubeSimulator:
+    serial_port_1 = serial.Serial('/dev/pts/6')
+    csv_data = pd.read_csv('datos_corregidos.csv')
+    iteration = 0
 
-def generate_orientation():
-    return csv_data['gx'].loc[iteration]
+    def generate_orientation(self):
+        return [self.csv_data['gx'].loc[self.iteration], self.csv_data['gy'].loc[self.iteration], self.csv_data['gz'].loc[self.iteration]]
 
-def generate_altitude():
-    return csv_data['altura'].loc[iteration]
+    def generate_altitude(self):
+        return self.csv_data['altura'].loc[self.iteration]
 
-def generate_velocity():
-    return csv_data[''].loc[iteration]
+    def generate_velocity(self):
+        return self.csv_data[''].loc[self.iteration]
 
-def generate_acelerations():
-    return csv_data['ax'].loc[iteration]
+    def generate_acelerations(self):
+        return [self.csv_data['ax'].loc[self.iteration], self.csv_data['ay'].loc[self.iteration], self.csv_data['az'].loc[self.iteration]]
 
-def generate_temperature():
-    return csv_data['temperatura'].loc[iteration]
+    def generate_temperature(self):
+        return self.csv_data['temperatura'].loc[self.iteration]
 
-def generate_pressure():
-    return csv_data['presion'].loc[iteration]
+    def generate_pressure(self):
+        return self.csv_data['presion'].loc[self.iteration]
 
-def generate_position():
-    return [random.uniform(-40, 40), random.uniform(-40, 40)]
+    def generate_position(self):
+        return [random.uniform(-40, 40), random.uniform(-40, 40)]
 
-def generate_battery():
-    return random.randint(0, 100)
+    def generate_battery(self):
+        return random.randint(0, 100)
 
-def generate_phase():
-    return random.randint(0, 3)
+    def generate_phase(self):
+        return random.randint(0, 3)
 
-def generate_all_data():
-    return [
-        generate_orientation(),
-        generate_altitude(),
-        generate_velocity(),
-        generate_acelerations(), 
-        generate_temperature(),
-        generate_pressure(),
-        generate_position(),
-        generate_battery(),
-        generate_phase()
-    ]
+    def get_medition(self):
+        return self.csv_data['medicion'].loc[self.iteration]
 
-instructions = {
-    # Acctions
-    'str': None, 
-    'abr': None,
-    'rst': None,
-    'fin': None,
-    # Data
-    'ori': generate_orientation,
-    'alt': generate_altitude,
-    'vel': generate_velocity,
-    'acl': generate_acelerations,
-    'tem': generate_temperature,
-    'pre': generate_pressure,
-    'pos': generate_position,
-    'bat': generate_battery,
-    'pha': generate_phase,
-    'all': generate_all_data,
-}
+    def get_time(self):
+        return self.csv_data['tiempo'].loc[self.iteration]
 
-it = 0xFE
-do = 0xCC
-dd = 0xDD
-l = None
-rssi = hex(random.randint(-120, -60))
-i = None
-m = None
-ft = 0xEF
+    def generate_all_data(self):
+        self.iteration += 1
+        return [
+            self.get_medition(),
+            self.get_time(),
+            self.generate_altitude(),
+            self.generate_temperature(),
+            self.generate_pressure(),
+            self.generate_acelerations(), 
+            self.generate_orientation(),
+            #generate_velocity(),
+            #generate_position(),
+            #generate_battery(),
+            #generate_phase()
+        ]
 
-while True:
-    if serial_port_1.in_waiting > 0:
-        data = serial_port_1.readline().decode('utf-8', errors='ignore').rstrip()
-        print(data)
+    instructions = {
+        # Acctions
+        'str': None, 
+        'abr': None,
+        'rst': None,
+        'fin': None,
+        # Data
+        'ori': generate_orientation,
+        'alt': generate_altitude,
+        'vel': generate_velocity,
+        'acl': generate_acelerations,
+        'tem': generate_temperature,
+        'pre': generate_pressure,
+        'pos': generate_position,
+        'bat': generate_battery,
+        'pha': generate_phase,
+        'all': generate_all_data,
+    }
 
-        data_separared = data.split()
-        if data_separared[0] == 'pq':
-            print(f'command recived: {data_separared[1]}')
-            for key, value in instructions.items():
-                if data_separared[1] == key:
-                    i = key
-                    m = value()
-                    response = f'{hex(it)} {hex(do)} {hex(dd)} {rssi} {i} {m} {hex(ft)}'
-                    print(f'response: {response}\n')
-                    iteration += 1
-                    break
-        else:
+    it = 0xFE
+    do = 0xCC
+    dd = 0xDD
+    l = None
+    rssi = hex(random.randint(-120, -60))
+    i = None
+    m = None
+    ft = 0xEF
+
+
+    '''
+    while True:
+        if serial_port_1.in_waiting > 0:
+            data = serial_port_1.readline().decode('utf-8', errors='ignore').rstrip()
             print(data)
-            print('nop')
+
+            data_separared = data.split()
+            if data_separared[0] == 'pq':
+                print(f'command recived: {data_separared[1]}')
+                for key, value in instructions.items():
+                    if data_separared[1] == key:
+                        i = key
+                        m = value()
+                        response = f'{hex(it)} {hex(do)} {hex(dd)} {rssi} {i} {hex(it)} {m} {hex(ft)} {hex(ft)}'
+                        serial_port_1.write(b'{response}\n')
+                        print(f'response: {response}\n')
+                        iteration += 1
+                        break
+            else:
+                print(data)
+                print('nop')
+    '''
--- request_handler.py
+++ request_handler.py
@@ -1,16 +1,44 @@
 import requests
 import serial
 from time import sleep
+import pocketqube_simulator
 
 url = 'http://localhost:8000/graphs/medition'
 body = {
-    'medition': 1,
-    'time': None
+    'medition': None,
+    'time': None,
+    'altitude': None,
+    'temperature': None,
+    'pressure': None,
+    'aceleration_x': None,
+    'aceleration_y': None,
+    'aceleration_z': None,
+    'gyro_x': None,
+    'gyro_y': None,
+    'gyro_z': None,
+    'battery': None,
     }
 
-#r = requests.get(url, body)
-#print(r.text)
+pq = pocketqube_simulator.PocketQubeSimulator()
 
-port = serial.Serial('/dev/pts/11')
-port.write(b'pq tem\n')
+for _ in range(1000):
+    data = pq.generate_all_data()
+
+    body['medition'] = data[0]
+    body['time'] = data[1]
+    body['altitude'] = data[2]
+    body['temperature'] = data[3]
+    body['pressure'] = data[4]
+    body['aceleration_x'] = data[5][0]
+    body['aceleration_y'] = data[5][1]
+    body['aceleration_z'] = data[5][2]
+    body['gyro_x'] = data[6][0]
+    body['gyro_y'] = data[6][1]
+    body['gyro_z'] = data[6][2]
+    body['battery'] = 99
+
+    r = requests.get(url, body)
+    print(r.text)
+    sleep(0.5)
+
 
